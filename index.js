ce595e385c43e9145085fc998917b5f276eccd84
eggyo <eggyoyo4859@gmail.com>
2016-07-16 07:47:10 +0700

    v.1.0

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+Instagram = require('instagram-node-lib');
+
+Instagram.set('client_id', 'f0b52d7c5aa6422ca15e4822d1ebd830');
+Instagram.set('client_secret', '71ffe85517bf49998481021048b5e2dc');
+Instagram.set('callback_url', 'http://eggyo-ig-sub.herokuapp.com/webhook');
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +18,22 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/testPop', function(request, response) {
+  Instagram.media.popular();
+});
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'myVerifyToken') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
 app.post('/webhook', function (request, response) {
   var data = response.body;
   console.log("post data received :"+data);
